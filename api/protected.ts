ac9e8bfc9045e176c52bbf46dd7e661ef28738ef
Mudit Ameta <zeusdeux@gmail.com>
2019-08-27 22:12:39 +0200

    Add protected route handler

---
+++ api/protected.ts
@@ -0,0 +1,48 @@
+import { NowRequest, NowResponse } from '@now/node'
+import { STATUS_CODES } from 'http'
+import { v4 as uuidV4 } from 'uuid'
+import { CustomError, ErrorType, getErrorHandler } from '../errors/CustomError'
+import { verify as verifyToken } from '../models/Token'
+import { match } from '../utils/match'
+
+function getToken(req: NowRequest): string {
+  const token = (req.headers.authorization || '').trim().split(/^(?:b|B)earer +/)[1]
+
+  return token ? token : typeof req.query.token === 'string' ? req.query.token : '<INVALID TOKEN>'
+}
+
+export default async function(req: NowRequest, res: NowResponse) {
+  const token = getToken(req)
+  const requestId = uuidV4()
+  const handleError = getErrorHandler(res, requestId)
+
+  console.log('Begin requestId ->', requestId) // tslint:disable-line:no-console
+
+  if (!token) {
+    handleError(new CustomError({ type: ErrorType.Unauthorized }))
+  } else {
+    match(await verifyToken(token), {
+      left: handleError,
+      right: async tokenPayload => {
+        const status = 200
+        const response = {
+          requestId,
+          payload: tokenPayload,
+          message: 'Successfully verified token! Yay!'
+        }
+
+        console.log('Token verified', process.env.NODE_ENV !== 'production' ? response : '.') // tslint:disable-line:no-console
+
+        const stringifiedResponse = JSON.stringify(response) + '\n'
+
+        res.writeHead(status, STATUS_CODES[status], {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(stringifiedResponse)
+        })
+        res.end(stringifiedResponse)
+      }
+    })
+  }
+
+  console.log('End requestId ->', requestId) // tslint:disable-line:no-console
+}
